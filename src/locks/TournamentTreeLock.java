369b97bbd3f31efa0696e4b9978e38e507bfebf9
Ethan <ethanhchen01@gmail.com>
2025-06-21 18:07:58 -0500

    mostly finished, need to figure out how to start the lock

--- src/locks/TournamentTreeLock.java
+++ src/locks/TournamentTreeLock.java
@@ -7,7 +7,31 @@ public class TournamentTreeLock {
         nodes = new PetersonLock[numProcesses-1];
     }
 
-    public void acquire(Runnable callback) {
-        //assign the process to one of the leaf nodes that is available. need to figure out how to find out which node is available
+    public void acquireLock(Thread[] processes) {
+        if (processes.length != nodes.length+1) throw new IllegalArgumentException("array size does not match");
+        
+        if (processes.length == 1) {
+            acquire(0, 1, 0);
+            processes[0].start();
+            release(0, 1, 0);
+            return;
+        }
+        for (int i = 0; i < processes.length; i++) {
+            //todo
+        }
+    }
+
+    private void acquire(int id, int total, int offset) {
+        if (total == 1) return;
+        int instance = id / 2;
+        nodes[offset+instance].acquireLock(id % 2);
+        acquire(instance, total/2, offset + total/2);
+    }
+
+    private void release(int id, int total, int offset) {
+        if (total == 1) return;
+        int instance = id / 2;
+        release(instance, total/2, offset + total/2);
+        nodes[offset+instance].releaseLock(id % 2);
     }
 }
